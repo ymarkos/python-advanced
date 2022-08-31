74d715c0425329e0b2610f9314fbabe236e7d27c
Yana Markos <yn.mrks@gmail.com>
2022-08-31 17:12:05 +0200

    1st exercise - tuples and sets lab

---
+++ 02-tuples-and-sets-a-lab/01_count_same_values.py
@@ -0,0 +1,17 @@
+'''
+occurrences_dict
+loop values:
+    occurrences_dict[value] +=1
+'''
+
+number_strings = input()
+occurrences_count = {}
+numbers = [float(num) for num in number_strings.split()]
+for number in numbers:
+    if number not in occurrences_count:
+        occurrences_count[number] = 0
+    occurrences_count[number] +=1
+
+
+for key, value in occurrences_count.items():
+    print(f'{key} - {value} times')
