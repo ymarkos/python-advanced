2183c0a051e6b3787dd1885827344e34b7b08e8d
Yana Markos <yn.mrks@gmail.com>
2022-09-13 12:15:06 +0200

    Modified exercise 1

--- 01-stacks-queues-b-exercises/01_reverse_numbers.py
+++ 01-stacks-queues-b-exercises/01_reverse_numbers.py
@@ -1,4 +1,11 @@
+#1st option
 data = [int(x) for x in input().split()]
-reversed_data = []
+
 while data:
     print(data.pop(), end=' ')
+#2nd option
+line = list(input().split())
+reversed_line = []
+while line:
+    reversed_line.append(line.pop())
+print(' '.join(reversed_line))
