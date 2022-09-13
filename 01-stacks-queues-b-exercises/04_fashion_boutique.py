999c013e4d7b81f4ab47e9cb381e409869f40300
Yana Markos <yn.mrks@gmail.com>
2022-09-13 14:05:06 +0200

    Modified exercise 4

--- 01-stacks-queues-b-exercises/04_fashion_boutique.py
+++ 01-stacks-queues-b-exercises/04_fashion_boutique.py
@@ -16,17 +16,20 @@ print the count_rack
 
 """
 
-stack_clothes = [int(piece) for piece in input().split()]
+clothes_stack = [int(el) for el in input().split()]
 
-rack_capacity = int(input())
-current_rack_capacity = rack_capacity
+capacity_rack = int(input())
+current_capacity = capacity_rack
 count_rack = 1
-while len(stack_clothes) >= 1:
-    current_piece = stack_clothes[-1]
-    if current_piece > current_rack_capacity:
+
+
+while len(clothes_stack) >= 1:
+    current_item = clothes_stack[-1]
+    if current_item > current_capacity:
+        current_capacity = capacity_rack
         count_rack += 1
-        current_rack_capacity = rack_capacity
+        continue
     else:
-        current_rack_capacity -= current_piece
-        stack_clothes.pop()
+        current_capacity -= current_item
+        clothes_stack.pop()
 print(count_rack)
