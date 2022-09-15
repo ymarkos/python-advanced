079cbda62ab084f05ede3b02773296624598710c
Yana Markos <yn.mrks@gmail.com>
2022-09-15 14:16:41 +0200

    Modified ex. 01 with functions

--- 04-multidimentional-structures-a-lab/01_sum_matrix_elements.py
+++ 04-multidimentional-structures-a-lab/01_sum_matrix_elements.py
@@ -1,12 +1,22 @@
-rows, columns = [int(x) for x in input().split(', ')]
-matrix = []
-matrix_sum = 0
-for _ in range(rows):
-    row = [int(x) for x in input().split(', ')]
-    matrix.append(row)
-    matrix_sum += sum(row)
+def read_matrix():
+    rows, columns = [int(x) for x in input().split(', ')]
+    matrix = []
+    for _ in range(rows):
+        row = [int(x) for x in input().split(', ')]
+        matrix.append(row)
+    return matrix
 
 
+matrix = read_matrix()
 
-print(matrix_sum)
+
+def sum_matrix():
+    total_sum = 0
+    for el in matrix:
+        total_sum += sum(el)
+    return total_sum
+
+
+tot_sum = sum_matrix()
+print(tot_sum)
 print(matrix)
