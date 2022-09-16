46fda426ef4bf975d2d52e781cd899272a44f63f
Yana Markos <yn.mrks@gmail.com>
2022-09-16 10:29:02 +0200

    Added 1 more solution ex 02

--- 02-tuples-and-sets-b-exercises/02_set_of_elements.py
+++ 02-tuples-and-sets-b-exercises/02_set_of_elements.py
@@ -3,6 +3,19 @@ length_a = int(a)
 length_b = int(b)
 set_a = {input() for _ in range(length_a)}
 set_b = {input() for _ in range(length_b)}
-set_intersection = set_a & set_b
+set_intersection = set_a.intersection(set_b)
 [print(el) for el in set_intersection]
 
+
+# first_length, second_length = [int(x) for x in input().split()]
+# first_set = set()
+# second_set = set()
+# for _ in range(first_length):
+#     char = input()
+#     first_set.add(int(char))
+#
+# for _ in range(second_length):
+#     char = input()
+#     second_set.add(int(char))
+# intersection = first_set & second_set
+# [print(el) for el in intersection]
