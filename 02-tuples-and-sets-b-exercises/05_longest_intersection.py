87c2a613b2c7596b008aba571bd8e4752632f44d
Yana Markos <yn.mrks@gmail.com>
2022-09-16 12:31:50 +0200

    Added ex 05

--- 02-tuples-and-sets-b-exercises/05_longest_intersection.py
+++ 02-tuples-and-sets-b-exercises/05_longest_intersection.py
@@ -1,7 +1,25 @@
-num = int(input())
+num_intr = int(input())
 
-
-for _ in range(num):
-    data = input().split('-')
-    first_start, first_end = data[0].split(',')
-    second_start, second_end = data[1].split(',')
+intersections = {}
+for intersection in range(num_intr):
+    a, b = input().split('-')
+    a_st, a_end = a.split(',')
+    start_a = int(a_st)
+    end_a = int(a_end)
+    current_a = set()
+    for el in range(start_a, end_a + 1):
+        current_a.add(el)
+    b_st, b_end = b.split(',')
+    start_b = int(b_st)
+    end_b = int(b_end)
+    current_b = set()
+    for el2 in range(start_b, end_b + 1):
+        current_b.add(el2)
+    current_intersection = current_a & current_b
+    current_intersection = tuple(current_intersection)
+    if current_intersection not in intersections:
+        intersections[current_intersection] = len(current_intersection)
+max_value = max(intersections.values())
+for key, value in intersections.items():
+    if value == max_value:
+        print(f'Longest intersection is {list(key)} with length {value}')
