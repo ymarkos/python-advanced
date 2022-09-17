0df576e7ba1ea3d0880c0f96559e24ff111195c4
Yana Markos <yn.mrks@gmail.com>
2022-09-17 20:46:07 +0200

    Added one more solution ex 02

--- 04-multidimentional-structures-a-lab/02_even_matrix.py
+++ 04-multidimentional-structures-a-lab/02_even_matrix.py
@@ -14,3 +14,16 @@ print(even_matrix)
 # rows = int(input())
 # matrix = [[int(x) for x in input().split(', ')] for _ in range(rows)]
 # print([[x for x in row if x % 2 == 0] for row in matrix])
+
+
+# num = int(input())
+# even_matrix = []
+#
+# for _ in range(num):
+#     even = []
+#     for el in [int(el) for el in input().split(', ')]:
+#         if el % 2 == 0:
+#             even.append(el)
+#     even_matrix.append(even)
+#
+# print(even_matrix)
