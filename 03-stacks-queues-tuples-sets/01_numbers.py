d678e6d07021a435ae3cc24b4173fde87f0684a0
Yana Markos <yn.mrks@gmail.com>
2022-09-20 11:08:02 +0200

    Modified ex 01

--- 03-stacks-queues-tuples-sets/01_numbers.py
+++ 03-stacks-queues-tuples-sets/01_numbers.py
@@ -5,33 +5,23 @@ num_command = int(input())
 for _ in range(num_command):
     data = input().split()
     command = data[0:2]
-    if data[0] == 'Check':
-        if set_a.issubset(set_b) or set_b.issubset(set_a):
-            print(True)
-        else:
-            print(False)
-    elif command == ['Add', 'First']:
-        numbers = [int(x) for x in (data[2:])]
-        for num in numbers:
-            set_a.add(num)
-    elif command == ['Add', 'Second']:
-        numbers = [int(x) for x in (data[2:])]
-        for num in numbers:
-            set_b.add(num)
-    elif command == ['Remove', 'First']:
-        numbers = [int(x) for x in (data[2:])]
-        for num in numbers:
-            if num in set_a:
-                set_a.remove(num)
-    elif command == ['Remove', 'Second']:
-        numbers = [int(x) for x in (data[2:])]
-        for num in numbers:
-            if num in set_b:
-                set_b.remove(num)
-set_a = sorted(set_a)
-set_b = sorted(set_b)
+    if command == ['Add', 'First']:
+        [set_a.add(int(x)) for x in data[2:]]
 
-print(", ".join(map(str, set_a)))
-print(", ".join(map(str, set_b)))
+    elif command == ['Add', 'Second']:
+        [set_b.add(int(x)) for x in data[2:]]
+
+    elif command == ['Remove', 'First']:
+        [set_a.remove(int(x)) for x in data[2:] if int(x) in set_a]
+
+    elif command == ['Remove', 'Second']:
+        [set_b.remove(int(x)) for x in data[2:] if int(x) in set_b]
+
+    else:
+        print(set_a.issubset(set_b) or set_b.issubset(set_a))
+
+
+print(*sorted(set_a), sep=', ')
+print(*sorted(set_b), sep=', ')
 
 
