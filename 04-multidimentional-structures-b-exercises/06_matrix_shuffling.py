a3231f43bee68d4d88eb6f4791a7ac027272cbd6
Yana Markos <yn.mrks@gmail.com>
2022-09-26 13:57:04 +0200

    Added exercise 06

---
+++ 04-multidimentional-structures-b-exercises/06_matrix_shuffling.py
@@ -0,0 +1,29 @@
+def is_outside(row, col, rows, cols):
+    return row < 0 or col < 0 or row >= rows or col >= cols
+
+
+rows, cols = [int(x) for x in input().split()]
+
+matrix = []
+for _ in range(rows):
+    matrix.append(input().split())
+
+
+while True:
+    line = input()
+    if line == 'END':
+        break
+
+    commands_args = line.split()
+    if len(commands_args) != 5 or commands_args[0] != 'swap':
+        print('Invalid input!')
+        continue
+    row1, col1, row2, col2 = [int(x) for x in commands_args[1:]]
+
+    if is_outside(row1, col1, rows, cols) or is_outside(row2, col2, rows, cols):
+        print('Invalid input!')
+        continue
+
+    matrix[row1][col1], matrix[row2][col2] = matrix[row2][col2], matrix[row1][col1]
+    for row in matrix:
+        print(*row)
